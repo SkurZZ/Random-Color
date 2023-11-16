5d6525cc03c4be9980b67173e63697901e440a8c
Fábio <fabiiohenrique.or@gmail.com>
2023-11-15 21:03:00 -0300

    Added comments and adjusted styling on the page.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const main = document.querySelector('main');
-const btn = document.getElementById('clickMe');
+const mainElement = document.querySelector('main');
+const clickButton = document.getElementById('clickMe');
 const hexDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-const hexText = document.getElementById("showHex")
+const hexDisplay = document.getElementById('showHex');
 
-btn.addEventListener('click', function () {
-    let cor = "#";
+clickButton.addEventListener('click', function () {
+    let randomColor = "#";
     for (let i = 0; i < 6; i++) {
-        cor += hexDigits[Math.floor(Math.random() * 16)];
+        randomColor += hexDigits[Math.floor(Math.random() * 16)];
     }
 
-    console.log(cor);
-    hexText.innerText = `${cor}`
-    main.style.backgroundColor = cor;
+    console.log(randomColor);
+    hexDisplay.innerText = randomColor;
+    mainElement.style.backgroundColor = randomColor;
 });
